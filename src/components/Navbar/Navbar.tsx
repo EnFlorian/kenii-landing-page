00c94fcbff6925c9e62243a469660d5552c1aae5
EnFlorian <33962956+EnFlorian@users.noreply.github.com>
2022-09-08 00:19:21 +0200

    Feat: Added mobile styling

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,15 @@
-import { logo, links } from "../../data/navbar";
+import { logo, links, button } from "../../data/navbar";
 import { useUiStore } from "../../state/store";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoCloseSharp } from "react-icons/io5";
 import "./Navbar.scss";
 
 const Navbar = () => {
-  const { openMobileMenu } = useUiStore((state) => state);
+  const { openMobileMenu, isMobileMenuOpen, closeMobileMenu } = useUiStore(
+    (state) => state
+  );
 
-  const renderedLinks = links.map((link, index) => (
+  const renderedDesktopLinks = links.map((link, index) => (
     <li className="navbar__link" key={index}>
       <a target="_blank" rel="noopener noreferrer" href={link.link}>
         {link.name}
@@ -13,21 +17,57 @@ const Navbar = () => {
     </li>
   ));
 
+  const renderedMobileLinks = links.map((link, index) => (
+    <li className="navbar__mobile-link" key={index}>
+      <a target="_blank" rel="noopener noreferrer" href={link.link}>
+        {link.name}
+      </a>
+    </li>
+  ));
+
   return (
-    <section className="navbar container">
-      <div className="navbar__logo-wrapper">
-        <a className="navbar__logo-text" href={logo.link}>
-          {logo.name}
-        </a>
-      </div>
-      <ul className="navbar__links">
-        {renderedLinks}
-        <li>
-          <button className="navbar__button">
-            <span className="navbar__button-text">Get in touch</span>
+    <section className="navbar">
+      <div className="navbar__wrapper container">
+        <div className="navbar__logo-wrapper">
+          <a className="navbar__logo-text" href={logo.link}>
+            {logo.name}
+          </a>
+        </div>
+        <ul className="navbar__links">
+          {renderedDesktopLinks}
+          <li>
+            <button className="navbar__button">
+              <span className="navbar__button-text">{button.name}</span>
+            </button>
+          </li>
+        </ul>
+        <div className="navbar__mobile-open-button" onClick={openMobileMenu}>
+          <li className="navbar__icon">
+            <GiHamburgerMenu />
+          </li>
+        </div>
+        <div
+          className={
+            isMobileMenuOpen
+              ? "navbar__mobile-menu"
+              : "navbar__mobile-menu hidden"
+          }
+        >
+          <ul>{renderedMobileLinks}</ul>
+          <button className="navbar__mobile-button">
+            <span className="navbar__mobile-button-text">{button.name}</span>
           </button>
-        </li>
-      </ul>
+          {/* close button */}
+          <div
+            className="navbar__mobile-close-button"
+            onClick={closeMobileMenu}
+          >
+            <li className="navbar__icon">
+              <IoCloseSharp />
+            </li>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
